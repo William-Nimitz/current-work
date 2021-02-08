59fd8a5d69cf36bbb1b06cc062ad6f7c2bb620a8
scraper <71530871+William-Nimitz@users.noreply.github.com>
2021-02-08 15:12:19 +0800

    [Add] EM-80 EM-85

--- src/app/components/new-campaign/new-campaign.component.ts
+++ src/app/components/new-campaign/new-campaign.component.ts
@@ -1,4 +1,4 @@
-./src/app/components/new-campaign/new-campaign.component.ts //14630
+./src/app/components/new-campaign/new-campaign.component.ts //5476
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { NgbActiveModal, NgbModal } from '@ng-bootstrap/ng-bootstrap';
