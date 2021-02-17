9d0275465580f143fb430193af663ec4f18cc032
scraper <71530871+William-Nimitz@users.noreply.github.com>
2021-02-18 03:12:15 +0800

    [Add] XI-257 XI-259 XI-252

--- src/app/services/conversation.service.ts
+++ src/app/services/conversation.service.ts
@@ -1,4 +1,4 @@
-./src/app/services/conversation.service.ts //17103
+./src/app/services/conversation.service.ts //29882
 import { Injectable } from '@angular/core';
 import { Conversation } from '../classes/conversation';
 import { Language } from '../classes/language';
