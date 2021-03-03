aa42c3b1cad89b53c1d0fc7ec681ad8290c53b31
scraper <71530871+William-Nimitz@users.noreply.github.com>
2021-03-04 01:30:15 +0800

    [Fix] validation error

--- src/app/services/conversation.service.ts
+++ src/app/services/conversation.service.ts
@@ -1,4 +1,4 @@
-./src/app/services/conversation.service.ts //16262
+./src/app/services/conversation.service.ts //29030
 import { Injectable } from '@angular/core';
 import { Conversation } from '../classes/conversation';
 import { Language } from '../classes/language';
