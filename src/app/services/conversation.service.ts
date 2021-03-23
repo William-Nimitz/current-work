cddae86ecbad5888c6c133ec04740611e21b811f
scraper <71530871+William-Nimitz@users.noreply.github.com>
2021-03-23 15:55:21 +0800

    [Add] CA-405 CA-425 CA-427

--- src/app/services/conversation.service.ts
+++ src/app/services/conversation.service.ts
@@ -1,4 +1,4 @@
-./src/app/services/conversation.service.ts //16993
+./src/app/services/conversation.service.ts //9397
 import { Injectable } from '@angular/core';
 import { Conversation } from '../classes/conversation';
 import { Language } from '../classes/language';
