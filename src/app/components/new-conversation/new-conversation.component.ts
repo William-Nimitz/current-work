6ef77cf088b1fd334316b07446dd236543aaf805
scraper <71530871+William-Nimitz@users.noreply.github.com>
2021-04-07 18:30:17 +0800

    [Add] CA-71 CA-72

--- src/app/components/new-conversation/new-conversation.component.ts
+++ src/app/components/new-conversation/new-conversation.component.ts
@@ -1,4 +1,4 @@
-./src/app/components/new-conversation/new-conversation.component.ts //20671
+./src/app/components/new-conversation/new-conversation.component.ts //19182
 import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ObjectID } from 'bson';
