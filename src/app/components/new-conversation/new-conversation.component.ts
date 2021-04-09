52ad492b066e7962985d2121d9f6febf2e110239
scraper <71530871+William-Nimitz@users.noreply.github.com>
2021-04-09 09:07:20 +0800

    [Feat] Business Wallet Setting

--- src/app/components/new-conversation/new-conversation.component.ts
+++ src/app/components/new-conversation/new-conversation.component.ts
@@ -1,4 +1,4 @@
-./src/app/components/new-conversation/new-conversation.component.ts //13212
+./src/app/components/new-conversation/new-conversation.component.ts //21346
 import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ObjectID } from 'bson';
