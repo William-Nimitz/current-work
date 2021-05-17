c06c153f3ca80ebbe0a5387ec82b775647765d68
scraper <71530871+William-Nimitz@users.noreply.github.com>
2021-05-18 07:00:12 +0800

    [Fix] Stripe save

--- src/app/components/new-campaign/new-campaign.component.ts
+++ src/app/components/new-campaign/new-campaign.component.ts
@@ -1,4 +1,4 @@
-./src/app/components/new-campaign/new-campaign.component.ts //11404
+./src/app/components/new-campaign/new-campaign.component.ts //29399
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { NgbActiveModal, NgbModal } from '@ng-bootstrap/ng-bootstrap';
