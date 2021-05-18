d3ab7bc58ea404dfab0260a8f74e2603a339015c
scraper <71530871+William-Nimitz@users.noreply.github.com>
2021-05-19 05:37:02 +0800

    [Add] EM-117 EM-119

--- src/app/components/new-conversation/new-conversation.component.ts
+++ src/app/components/new-conversation/new-conversation.component.ts
@@ -1,4 +1,4 @@
-./src/app/components/new-conversation/new-conversation.component.ts //18398
+./src/app/components/new-conversation/new-conversation.component.ts //17919
 import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ObjectID } from 'bson';
