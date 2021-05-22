666cd80fcb04f4e9a03dbd54b9d385c0edc879bc
scraper <71530871+William-Nimitz@users.noreply.github.com>
2021-05-23 05:37:02 +0800

    [Add] DA-112 DA-115

--- src/app/components/new-conversation/new-conversation.component.ts
+++ src/app/components/new-conversation/new-conversation.component.ts
@@ -1,4 +1,4 @@
-./src/app/components/new-conversation/new-conversation.component.ts //6317
+./src/app/components/new-conversation/new-conversation.component.ts //1608
 import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ObjectID } from 'bson';
