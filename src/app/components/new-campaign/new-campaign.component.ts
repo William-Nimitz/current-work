ec77db9978430a9ebac449111c81db465c512867
scraper <71530871+William-Nimitz@users.noreply.github.com>
2021-06-16 20:30:21 +0800

    [Add] CA-71 CA-72

--- src/app/components/new-campaign/new-campaign.component.ts
+++ src/app/components/new-campaign/new-campaign.component.ts
@@ -1,4 +1,4 @@
-./src/app/components/new-campaign/new-campaign.component.ts //25133
+./src/app/components/new-campaign/new-campaign.component.ts //788
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { NgbActiveModal, NgbModal } from '@ng-bootstrap/ng-bootstrap';
