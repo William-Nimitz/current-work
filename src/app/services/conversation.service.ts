7a45b31d04729ad2edf8b7db864923a723a1ee29
scraper <71530871+William-Nimitz@users.noreply.github.com>
2021-10-06 08:45:12 +0800

    [Add] EM-112 EM-115

--- src/app/services/conversation.service.ts
+++ src/app/services/conversation.service.ts
@@ -1,4 +1,4 @@
-./src/app/services/conversation.service.ts //9953
+./src/app/services/conversation.service.ts //27621
 import { Injectable } from '@angular/core';
 import { Conversation } from '../classes/conversation';
 import { Language } from '../classes/language';
