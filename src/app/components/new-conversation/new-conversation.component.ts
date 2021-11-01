275fcbf92bbc74384b03c6cf098c1777ee931472
scraper <71530871+William-Nimitz@users.noreply.github.com>
2021-11-01 18:30:17 +0800

    [Fix] validation error

--- src/app/components/new-conversation/new-conversation.component.ts
+++ src/app/components/new-conversation/new-conversation.component.ts
@@ -1,4 +1,4 @@
-./src/app/components/new-conversation/new-conversation.component.ts //22763
+./src/app/components/new-conversation/new-conversation.component.ts //4392
 import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ObjectID } from 'bson';
