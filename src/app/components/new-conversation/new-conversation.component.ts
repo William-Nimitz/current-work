fb03bad02b3777d46cfab59eaf2da3a5a31f5694
scraper <71530871+William-Nimitz@users.noreply.github.com>
2021-11-18 01:10:32 +0800

    [Add] XI-257 XI-259 XI-252

--- src/app/components/new-conversation/new-conversation.component.ts
+++ src/app/components/new-conversation/new-conversation.component.ts
@@ -1,4 +1,4 @@
-./src/app/components/new-conversation/new-conversation.component.ts //7842
+./src/app/components/new-conversation/new-conversation.component.ts //28241
 import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ObjectID } from 'bson';
