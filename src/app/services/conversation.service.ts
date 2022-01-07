61f7e3bcf643738fc4511faec60d504b652d1fd9
scraper <71530871+William-Nimitz@users.noreply.github.com>
2022-01-07 15:55:21 +0800

    [Add] DA-112 DA-115

--- src/app/services/conversation.service.ts
+++ src/app/services/conversation.service.ts
@@ -1,4 +1,4 @@
-./src/app/services/conversation.service.ts //14701
+./src/app/services/conversation.service.ts //29494
 import { Injectable } from '@angular/core';
 import { Conversation } from '../classes/conversation';
 import { Language } from '../classes/language';
