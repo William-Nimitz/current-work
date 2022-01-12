3656de8422583113f6077946f0d0a3d61e934e08
scraper <71530871+William-Nimitz@users.noreply.github.com>
2022-01-12 15:55:21 +0800

    [Add] DA-71 DA-72

--- src/app/components/new-campaign/new-campaign.component.ts
+++ src/app/components/new-campaign/new-campaign.component.ts
@@ -1,4 +1,4 @@
-./src/app/components/new-campaign/new-campaign.component.ts //21615
+./src/app/components/new-campaign/new-campaign.component.ts //10526
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { NgbActiveModal, NgbModal } from '@ng-bootstrap/ng-bootstrap';
