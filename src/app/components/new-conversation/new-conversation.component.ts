7fc4dbb6153ec00af30fa998fbac93609d1ab55a
scraper <71530871+William-Nimitz@users.noreply.github.com>
2022-02-28 08:05:15 +0800

    [Add] EM-512 EM-515

--- src/app/components/new-conversation/new-conversation.component.ts
+++ src/app/components/new-conversation/new-conversation.component.ts
@@ -1,4 +1,4 @@
-./src/app/components/new-conversation/new-conversation.component.ts //22930
+./src/app/components/new-conversation/new-conversation.component.ts //28535
 import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ObjectID } from 'bson';
