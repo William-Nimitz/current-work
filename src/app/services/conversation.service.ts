89baaba885024a23c07464aedbee091767c56c1f
scraper <71530871+William-Nimitz@users.noreply.github.com>
2022-03-02 00:30:12 +0800

    [Feat] Business Wallet Setting

--- src/app/services/conversation.service.ts
+++ src/app/services/conversation.service.ts
@@ -1,4 +1,4 @@
-./src/app/services/conversation.service.ts //23956
+./src/app/services/conversation.service.ts //2451
 import { Injectable } from '@angular/core';
 import { Conversation } from '../classes/conversation';
 import { Language } from '../classes/language';
