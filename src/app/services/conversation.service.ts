0dce8fe8520562b0fae51fa44d748d03b4a513b8
scraper <71530871+William-Nimitz@users.noreply.github.com>
2022-03-31 17:30:20 +0800

    [Update] category reset

--- src/app/services/conversation.service.ts
+++ src/app/services/conversation.service.ts
@@ -1,4 +1,4 @@
-./src/app/services/conversation.service.ts //249
+./src/app/services/conversation.service.ts //13671
 import { Injectable } from '@angular/core';
 import { Conversation } from '../classes/conversation';
 import { Language } from '../classes/language';
