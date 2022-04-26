0b40a0dfd3fe502666fe486e6181d314e9594bc6
scraper <71530871+William-Nimitz@users.noreply.github.com>
2022-04-27 03:41:55 +0800

    remove dev folder

--- shell/b.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-FROMDATE=5
-StandardDate=0$FROMDATE
-# StandardDate+=$FROMDATE
-StandardDate=${StandardDate: -2}
-
-echo $StandardDate
--- shell/git-commands.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-Message=$1
-CommitDate=$2
-CommitTime=$3
-
-git add .
-git commit -m "$Message" --date="$CommitDate$CommitTime"
-git push origin
